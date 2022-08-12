191fcb37b931a78747d565f5319d10c1a565ec83
Sean-Green <56403918+Sean-Green@users.noreply.github.com>
2022-08-12 15:48:12 -0700

    Added refresh token logic

--- TownSuite.DataExchange.ConsoleExample/Program.cs
+++ TownSuite.DataExchange.ConsoleExample/Program.cs
@@ -11,7 +11,7 @@ var builder = new ConfigurationBuilder()
 IConfiguration config = builder.Build();
 
 // This example console application expects that the account has already been bootstrapped
-// by TownSuite and an access and refresh token has been given to the developer.
+// by TownSuite and an api account, access and refresh tokens have been given to the developer.
 // Alternatively TownSuite has given a full account and the developer has bootstrapped an
 // access and refresh tokens.
 
@@ -20,14 +20,16 @@ var httpClient = new HttpClient();
 httpClient.DefaultRequestHeaders.Add("User-Agent",
     "TSX-console-example/1.0 Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0");
 
-
 // Decode token and view contents such as expires in
-var handler = new JwtSecurityTokenHandler();
-var jsonToken = handler.ReadToken(authSettings.AuthToken);
-var tokenS = jsonToken as JwtSecurityToken;
-Console.WriteLine(jsonToken.ToString());
+if (!String.IsNullOrWhiteSpace(authSettings.AuthToken))
+{
+    var handler = new JwtSecurityTokenHandler();
+    var jsonToken = handler.ReadToken(authSettings.AuthToken);
+    var tokenS = jsonToken as JwtSecurityToken;
+    Console.WriteLine(jsonToken.ToString());
+}
 
-
+// Try to fetch data from an endpoint
 var client = new Client(httpClient);
 var result = await client.Get(
     authSettings.BaseUrl,
@@ -35,15 +37,24 @@ var result = await client.Get(
     $"/api/v1/Property/Assessment/{authSettings.ApiAccount}");
 if (result.statusCode != 200)
 {
-    // assume auth token expired 
+    // Assume the auth token expired and use the refresh token to get a new one
     var refreshResult = await client.Get(
         authSettings.BaseUrl,
         authSettings.RefreshToken,
-        $"/api/v1/auth/refresh");
-    
+        $"/api/v1/Auth/Refresh");
+    if (refreshResult.statusCode != 200)
+    {
+        Console.WriteLine("Something went wrong.");
+        return;
+    }
     Console.WriteLine(refreshResult.json);
-    Console.WriteLine("Use the new tokens");
-    return;
+
+    // Use the new auth token to fetch data from the endpoint
+    var tokenResp = JsonExtensions.DeserializeFromJson<TokenResponse>(refreshResult.json);
+    result = await client.Get(
+    authSettings.BaseUrl,
+    tokenResp.authToken,
+    $"/api/v1/Property/Assessment/{authSettings.ApiAccount}");
 }
 
 Console.WriteLine(result.json);
---
+++ TownSuite.DataExchange.ConsoleExample/TokenResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TownSuite.DataExchange.ConsoleExample
+{
+
+    public class TokenResponse
+    {
+        public string authToken { get; set; }
+        public string refreshToken { get; set; }
+        public bool mustCompleteTwoFactor { get; set; }
+    }
+}
